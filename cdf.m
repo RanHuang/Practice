6115d73fd294a4036e4867f754c8f7202147cf2c
Nick Huang <xjhznick@gmail.com>
2016-08-07 12:36:46 +0800

    Delete files form branch matlab

--- cdf.m
+++
@@ -1,9 +0,0 @@
-function [xTime,yPercentage]=cdf(initValue,step,endValue,sample); 
-xTime=[];
-yPercentage=[];
-totalNum=length(sample);
-for i=initValue:step:endValue
-    temp=length(find(sample<=i))/totalNum;
-    xTime=[xTime,i];
-    yPercentage=[yPercentage,temp];
-end
