0d58a72f9e627985286296480b3afbdb5b7e15ba
Nick Huang <xjhznick@gmail.com>
2019-06-21 22:07:01 +0800

    求解一个字符串中不含有重复字符的 最长子串 的长度（暴力搜索）

---
+++ src/leetcode/medium/LongestSubstring.java
@@ -0,0 +1,82 @@
+package leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ * 示例 2:
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *
+ * 示例 4:
+ * 输入: "au"
+ * 输出: 2
+ * 解释: 因为无重复字符的最长子串是 "au"，所以其长度为 2。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author nick
+ * @date 2019-06-21 星期五 21:33
+ **/
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String str1st = "abcabcbb";
+        int length1st = lengthOfLongestSubstring(str1st);
+        System.out.println(length1st);
+
+        String str2nd = "bbbbb";
+        int length2nd = lengthOfLongestSubstring(str2nd);
+        System.out.println(length2nd);
+
+        String str3rd = "au";
+        int length3rd = lengthOfLongestSubstring(str3rd);
+        System.out.println(length3rd);
+    }
+
+    /**
+     * 暴力搜索，通过Set判断是否遇到重复字符，出现第一个重复字符之前的子串为
+     * 无重复字符的子串
+     * @param
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() < 2) {
+            // 空串或仅包含一个字符的字符串
+            return s.length();
+        }
+        char[] chars = s.toCharArray();
+        int sLength = chars.length;
+        int maxLength = 0;
+
+        Set<Character> characterSet = new HashSet<>(10);
+        for (int i = 0; i < sLength; i++) {
+            characterSet.clear();
+            for (int j = i; j < sLength; j++) {
+                if (characterSet.contains(chars[j])) {
+                    break;
+                } else {
+                    characterSet.add(chars[j]);
+                }
+            }
+            int length = characterSet.size();
+            maxLength = Math.max(length, maxLength);
+        }
+
+        return maxLength;
+    }
+}
