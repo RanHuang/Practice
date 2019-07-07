2399eb8ac449c6e56e5865ddfe57fa61e899f7d8
Nick Huang <xjhznick@gmail.com>
2019-07-07 23:31:18 +0800

    利用序列的有序性优化执行效率

--- src/leetcode/medium/NextPermutation.java
+++ src/leetcode/medium/NextPermutation.java
@@ -8,16 +8,16 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
- *
+ * <p>
  * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
- *
+ * <p>
  * 必须原地修改，只允许使用额外常数空间。
- *
+ * <p>
  * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
  * 1,2,3 → 1,3,2
  * 3,2,1 → 1,2,3
  * 1,1,5 → 1,5,1
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/next-permutation
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -37,7 +37,8 @@ public class NextPermutation {
     }
 
     @Test
-    public void debug() {
+    public void debugNextPermutation() {
+        assertTrue(Arrays.equals(nextPermutation(new int[]{1, 5, 1}), new int[]{5, 1, 1}));
         assertTrue(Arrays.equals(nextPermutation(new int[]{1, 3, 2}), new int[]{2, 1, 3}));
         assertTrue(Arrays.equals(nextPermutation(new int[]{2, 3, 1}), new int[]{3, 1, 2}));
         assertTrue(Arrays.equals(nextPermutation(new int[]{5, 2, 4, 3}), new int[]{5, 3, 2, 4}));
@@ -76,24 +77,19 @@ public class NextPermutation {
             // 方法2. 重新从小到大排序
             //Arrays.sort(nums);
         } else {
-            //取从j...length - 1中找到比nums[j-1]大的最小值位置记为s,
-            // 交换s与j-1位置元素，
-            // 对j...length-1位置的元素重新排序
-            int min = Integer.MAX_VALUE;
-            int s = 0;
-            int index = j;
-            while (index < length) {
-                if (nums[index] > nums[j - 1] && nums[index] < min) {
-                    s = index;
-                    min = nums[index];
-                }
-                index++;
+            // 取从j...length - 1中找到比nums[j-1]大的最小值位置记为s；交换s与j-1位置元素；对j...length-1位置的元素重新排序
+
+            // 由于j...length-1已经按照降序排列，从右往左找到第一个比num[j-1]大的元素即可
+            int s = length - 1;
+            while (nums[s] <= nums[j - 1]) {
+                s--;
             }
             //
+            // 交换
             nums[j - 1] = nums[s] ^ nums[j - 1];
             nums[s] = nums[s] ^ nums[j - 1];
             nums[j - 1] = nums[s] ^ nums[j - 1];
-
+            // 反转/重新排序
             Arrays.sort(nums, j, length);
         }
         return nums;
