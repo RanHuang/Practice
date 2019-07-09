2eaa53464934f9dd494990820b3debac4fdba534
Nick Huang <xjhznick@gmail.com>
2019-07-09 12:28:39 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置（算法时间复杂度必须是 O(log n) 级别），采用二分搜索法确定target位置

--- src/leetcode/medium/Search.java
+++ src/leetcode/medium/Search.java
@@ -6,20 +6,16 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
- * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
- * 你的算法时间复杂度必须是 O(log n) 级别。
- * 如果数组中不存在目标值，返回 [-1, -1]。
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
  * @author nick
  * @date 2019-07-09 星期二 12:13
  **/
-public class SearchRange {
+public class Search {
     /**
-     * 要求算法时间复杂度必须是 O(log n) 级别，使用二分查找法确定target位置
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * * 你的算法时间复杂度必须是 O(log n) 级别。
+     * * 如果数组中不存在目标值，返回 [-1, -1]。
+     * <p>
+     * 使用二分查找法确定target位置，确保时间复杂度未O(long n)
      *
      * @param nums
      * @param target
