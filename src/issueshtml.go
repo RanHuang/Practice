36e80a47e4f017eb64bc811553b9f0bb7e8c1736
Nick Huang <xjhznick@gmail.com>
2019-09-03 19:06:34 +0800

    使用HTML模板抽取和输出Issues内容

---
+++ src/issueshtml.go
@@ -0,0 +1,45 @@
+/*
+以Html模板输出issues
+Example: $ ./issueshtml repo:golang/go is:open json decoder > issues.html
+*/
+package main
+import(
+    "os"
+    "log"
+    "html/template"
+    "github"
+)
+
+const temp = `
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+    <th>#</th>
+    <th>State</th>
+    <th>User</th>
+    <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+    <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+    <td>{{.State}}</td>
+    <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+    <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`
+
+// 解析模板
+var report = template.Must(template.New("issuelist").Parse(temp))
+
+func main() {
+    result, err := github.SearchIssues(os.Args[1:])
+    if err != nil {
+        log.Fatal("Query Issues from GitHub failed.", err)
+    }
+
+    if err := report.Execute(os.Stdout, result); err != nil {
+        log.Fatal(err)
+    }
+}
